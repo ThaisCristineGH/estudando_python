a716eb4da859e13f239891d725325194eb3a8f30
Thais Cristine <contatotcristine@gmail.com>
2026-01-21 12:34:45 -0300

    Add files via upload

---
+++ strings_regex/006_f_string.py
@@ -0,0 +1,15 @@
+# 🔍 f-string
+
+#exemplo 1
+nome = "Python"
+nivel = "Avançado"
+print(f"Estou estudando {nome} no nível {nivel}.")
+# Saída / exemplo 1: Estou estudando Python no nível Avançado.
+
+
+#exemplo 2
+estudante = "Pedro"
+nota = 10
+mensagem = f"{estudante} tirou a nota {nota}!"
+print(mensagem)
+# Saída / exemplo 2: Pedro tirou nota 10!
